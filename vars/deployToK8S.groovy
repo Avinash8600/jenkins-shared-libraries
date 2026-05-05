55ccc51f19e5190bc642e6527894a13ea3c4d2e2
Avinash Wagh <avinashwagh745@gmail.com>
2026-05-05 13:55:05 +0530

    Update deployment commands for todo-app

--- vars/deployToK8S.groovy
+++ vars/deployToK8S.groovy
@@ -6,8 +6,8 @@ def call()
   kubectl apply -f k8s-deploy/namespace.yml
   kubectl apply -f k8s-deploy/
 
-  kubectl rollout restart deployment/todo-app -n todo-ns
-  kubectl rollout status deployment/todo-app -n todo-ns
+  kubectl rollout restart deployment.apps/todo-app-deployment -n todo-ns
+  kubectl rollout status deployment.apps/todo-app-deployment -n todo-ns
   echo "App deployed successfully 🚀"
   """
                 
