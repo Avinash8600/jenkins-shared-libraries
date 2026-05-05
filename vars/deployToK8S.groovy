4de8db92ccbcba4ce96bba1cd592bc425d6a23cb
Avinash Wagh <avinashwagh745@gmail.com>
2026-05-05 13:29:31 +0530

    Simplify Kubernetes deployment commands

--- vars/deployToK8S.groovy
+++ vars/deployToK8S.groovy
@@ -4,8 +4,7 @@ def call()
 
   sh """
   kubectl apply -f k8s-deploy/namespace.yml
-  kubectl apply -f k8s-deploy/deployment.yml
-  kubectl apply -f k8s-deploy/service.yml
+  kubectl apply -f k8s-deploy/
 
   kubectl rollout restart deployment/todo-app -n todo-ns
   kubectl rollout status deployment/todo-app -n todo-ns
